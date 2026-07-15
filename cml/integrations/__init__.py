9e934829e7e190776dc6a5c221f266ffd9175d17
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-15 18:52:10 +0300

    feat: export GuardrailDecisionV1 public API

--- cml/integrations/__init__.py
+++ cml/integrations/__init__.py
@@ -1,1 +1,29 @@
 """Optional CML integrations."""
+
+from cml.integrations.guardrail_decision import (
+    GUARDRAIL_DECISION_SCHEMA,
+    GuardrailDecisionClaimsV1,
+    GuardrailDecisionFinding,
+    GuardrailDecisionV1,
+    GuardrailDecisionVerificationResult,
+    canonical_guardrail_decision_json,
+    derive_guardrail_decision_id,
+    guardrail_decision_from_mapping,
+    issue_guardrail_decision,
+    load_guardrail_decision_json,
+    verify_guardrail_decision,
+)
+
+__all__ = [
+    "GUARDRAIL_DECISION_SCHEMA",
+    "GuardrailDecisionClaimsV1",
+    "GuardrailDecisionFinding",
+    "GuardrailDecisionV1",
+    "GuardrailDecisionVerificationResult",
+    "canonical_guardrail_decision_json",
+    "derive_guardrail_decision_id",
+    "guardrail_decision_from_mapping",
+    "issue_guardrail_decision",
+    "load_guardrail_decision_json",
+    "verify_guardrail_decision",
+]
