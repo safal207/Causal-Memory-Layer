958ed5f25ac9251129804c31d69d6634002cbc51
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-04 21:45:41 +0300

    feat(security): export gateway API

--- cml/transition_guard/__init__.py
+++ cml/transition_guard/__init__.py
@@ -1,4 +1,14 @@
 from .fixtures import asb15_case_from_evidence, build_forged_reasoning_fixture
+from .gateway import (
+    ActionEnvelope,
+    ExecutionReceipt,
+    GatewayStatus,
+    GatewayValidationError,
+    GuardedToolGateway,
+    InMemoryReplayStore,
+    ToolRegistry,
+    payload_digest,
+)
 from .graph import CausalTransitionGraph
 from .model import (
     GraphEdge,
@@ -13,16 +23,24 @@ from .model import (
 )
 
 __all__ = [
+    "ActionEnvelope",
     "CausalTransitionGraph",
+    "ExecutionReceipt",
+    "GatewayStatus",
+    "GatewayValidationError",
     "GraphEdge",
     "GraphNode",
     "GraphValidationError",
     "GuardVerdict",
+    "GuardedToolGateway",
+    "InMemoryReplayStore",
     "NodeKind",
     "Relation",
     "Space",
     "TemporalWindow",
+    "ToolRegistry",
     "TransitionEvidence",
     "asb15_case_from_evidence",
     "build_forged_reasoning_fixture",
+    "payload_digest",
 ]
